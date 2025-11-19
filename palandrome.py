15430e08b467ce0c74d539da4fcd1842b6dea520
prathik1330t <01fe24bca234@kletech.ac.in>
2025-11-19 15:02:05 +0530

    Update palandrome.py

--- palandrome.py
+++ palandrome.py
@@ -1,10 +1,31 @@
-s = input("Enter text: ")
+import sys
 
-rev = ""
-for ch in s:
-    rev = ch + rev     # building reversed string manually
+def palandrome(s):
+    rev = ""
+    for ch in s:
+        rev = ch + rev
+    if s == rev:
+        return "Palandrome"
+    else:
+        return "Not palandrome"
 
-if s == rev:
-    print("Palindrome")
-else:
-    print("Not a palindrome")
+
+if __name__ == "__main__":
+    print("=== Palandrome ===")
+    try:
+        if len(sys.argv) == 2:
+            # Case 1: Argument passed
+            s = sys.argv[1]
+        else:
+            # Case 2: Console input
+            s = input("Enter value: ")
+
+        print("=== Program parameters ===")
+        print("Input value :", s)
+
+        # Print the result
+        print(palandrome(s))
+
+    except ValueError:
+        print("Invalid input! Please enter valid values.")
+
